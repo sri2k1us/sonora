51746c262165b697006a05ff8d50f688fcf6e96f
Sriram Sirinvasan <sriram@cyverse.org>
2021-07-20 14:27:28 -0400

    update to react17 and next11

--- src/components/data/viewers/Editor.js
+++ src/components/data/viewers/Editor.js
@@ -6,7 +6,7 @@
  *
  */
 import React, { useEffect, useState } from "react";
-import { Controlled as CodeMirror } from "react-codemirror2";
+import { Controlled as CodeMirror } from "react17-codemirror2";
 
 import buildID from "components/utils/DebugIDUtil";
 
